880c82e2fa04272993016c37264f2963eba7c3a1
Asmaa <asmaa.aboasha@gmail.com>
2020-07-23 20:13:20 -0700

    Search input appends to page

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// // Initial array of cities 
-// var cities = ["Atlanta", "New York", "Chicago", "Seattle", "Portland"];
-
 // function displayWeatherInfo() {
 
 //     var city = $(this).attr("data-name")
 
 $("#add-city").on("click", function () {
+    event.preventDefault();
     var cityInput = $("#city-input").val();
     console.log(cityInput);
 
@@ -18,14 +16,42 @@ $("#add-city").on("click", function () {
        
         var cityDiv = $("<div class='city'>");
 
-        var temperature = response.Temp;
+        var cityName = response.name;
 
-        var pOne = $("<p>").text("Temperature: " + temperature);
+        var header = $("<h4>").text(cityName);
 
-        cityDiv.append(pOne);
+        cityDiv.append(header);
 
-        $("#city-section").append($(cityDiv));
+        $("#city-section").append(cityDiv);
+       
+       
+        // var temperature = response.Temp;
+
+        // var pOne = $("<p>").text("Temperature: " + temperature);
+
+        // cityDiv.append(pOne);
+
+        // $("#city-section").append(cityDiv);
        
     });
     
 });
+
+// Function for displaying city data
+// function renderButtons() {
+//     $("#buttons-view").empty();
+
+// // Looping through the array of cities
+// for (var i = 0; i < cities.length; i++) {
+
+//     var a = $("<button>");
+//           // Adding a class of city-btn to our button
+//           a.addClass("city-btn");
+//           // Adding a data-attribute
+//           a.attr("data-name", cities[i]);
+//           // Providing the initial button text
+//           a.text(cities[i]);
+//           // Adding the button to the buttons-view div
+//           $("#buttons-view").append(a);
+//         }
+// }
